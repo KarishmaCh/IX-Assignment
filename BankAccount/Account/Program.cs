21443ced6fc9fe431fe9d92c5508e4dc9a8dfef1
KarishmaCh <karishmac8899@gmail.com>
2023-02-21 15:47:46 +0530

    added daily limit of 1,00,000 for deposit amount, set daily limit of 50,000 for withdrawal  exception handling used

---
+++ BankAccount/Account/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Account
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            BankAccount bankAccountProgram = new BankAccount();
+            bankAccountProgram.RunBankAccountProgram();
+            Console.ReadLine();
+        }
+    }
+}
