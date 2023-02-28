28540802c589a594a8ea0d9a739bb887705b8fb3
karishma chavan <89963571+KarishmaCh@users.noreply.github.com>
2023-02-28 12:46:16 +0530

    added string methods file into String Method folder

---
+++ BankAccount/StringMethods/Duplicates.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class Duplicates
+
+{
+    static void Main(string[] args)
+    {
+        while (true)
+        {
+            Console.Write("Enter a string: ");
+            string inputString = Console.ReadLine();
+
+            string result = string.Empty;
+            foreach (char c in inputString)
+            {
+                if (result.IndexOf(c) == -1)
+                {
+                    result += c;
+                }
+            }
+
+            Console.WriteLine("After removing duplicates: " + result);
+
+            Console.Write("Do you want to continue? (y/n): ");
+            string response = Console.ReadLine().ToLower();
+
+            if (response != "y")
+            {
+                break;
+            }
+        }
+    }
+}
