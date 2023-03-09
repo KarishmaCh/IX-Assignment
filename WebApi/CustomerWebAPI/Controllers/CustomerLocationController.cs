8ad55e6e24c3ec9f3b71147c3bd6def3bf5f7d7e
karishma chavan <89963571+KarishmaCh@users.noreply.github.com>
2023-03-09 20:03:55 +0530

    Feature Swagger Configure added

--- WebApi/CustomerWebAPI/Controllers/CustomerLocationController.cs
+++ WebApi/CustomerWebAPI/Controllers/CustomerLocationController.cs
@@ -18,7 +18,14 @@ namespace CustomerWebAPI.Controllers
             new CustomerLocation { Id = 3, CustomerId = 2, Name = "Location 3", Address = "789 Oak St.", City = "Smallville", State = "TX", ZipCode = 54321 }
         };
 
+        /// <summary>
+        /// Gets the customer location with the specified ID.
+        /// </summary>
+        /// <param name="id">The ID of the customer location to retrieve.</param>
+        /// <returns>The customer location with the specified ID.</returns>
         [HttpGet("{id}")]
+        [SwaggerResponse(StatusCodes.Status200OK, "The customer location with the specified ID.", typeof(CustomerLocation))]
+        [SwaggerResponse(StatusCodes.Status404NotFound, "No customer location was found with the specified ID.")]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
         public ActionResult<CustomerLocation> Get(int id)
@@ -32,7 +39,17 @@ namespace CustomerWebAPI.Controllers
             return customerLocation;
         }
 
+
+        /// <summary>
+        /// Updates the specified customer location.
+        /// </summary>
+        /// <param name="id">The ID of the customer location to update.</param>
+        /// <param name="updatedLocation">The updated customer location.</param>
+        /// <returns>The updated customer location.</returns>
         [HttpPut("{id}")]
+        [SwaggerResponse(StatusCodes.Status200OK, "The updated customer location.", typeof(CustomerLocation))]
+        [SwaggerResponse(StatusCodes.Status400BadRequest, "The request model is invalid.")]
+        [SwaggerResponse(StatusCodes.Status404NotFound, "No customer location was found with the specified ID.")]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
@@ -61,8 +78,14 @@ namespace CustomerWebAPI.Controllers
             }
         }
 
-     
+        /// <summary>
+        /// Deletes the customer location with the specified ID.
+        /// </summary>
+        /// <param name="id">The ID of the customer location to delete.</param>
+        /// <returns>A 204 No Content response indicating that the customer location was deleted successfully.</returns>
         [HttpDelete("{id}")]
+        [SwaggerResponse(StatusCodes.Status204NoContent, "The customer location was deleted successfully.")]
+        [SwaggerResponse(StatusCodes.Status404NotFound, "No customer location was found with the specified ID.")]
         [ProducesResponseType(204)]
         [ProducesResponseType(404)]
         public IActionResult Delete(int id)
@@ -77,7 +100,16 @@ namespace CustomerWebAPI.Controllers
             return NoContent();
         }
 
+        /// <summary>
+        /// Deletes a specific customer location of a customer with the specified ID.
+        /// </summary>
+        /// <param name="customerId">The ID of the customer whose location is to be deleted.</param>
+        /// <param name="id">The ID of the location to delete.</param>
+        /// <returns>A 204 No Content response indicating that the customer location was deleted successfully.</returns>
         [HttpDelete("{customerId}/locations/{id}")]
+        [SwaggerResponse(StatusCodes.Status204NoContent, "The customer location was deleted successfully.")]
+        [SwaggerResponse(StatusCodes.Status404NotFound, "No customer or customer location was found with the specified IDs.")]
+        [SwaggerResponse(StatusCodes.Status409Conflict, "The customer has only one location, so the entire customer was deleted.")]
         [ProducesResponseType(204)]
         [ProducesResponseType(404)]
         [ProducesResponseType(409)]
